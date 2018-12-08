370b7500c5cd982e464c014c44510c659086e25c
zan <zan_zibert@hotmail.com>
2018-12-08 18:08:08 +0100

    Python 3 Tutorial for Beginners #20 - Bar Tab Calculator

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from space.planet import Planet
-from space.calc import planet_mass, planet_vol
+from bar_tab import Tab
 
-hoth = Planet('Retard', 2000, 5.5, "Yolo system")
+me = Tab()
 
-hoth_mass = planet_mass(hoth.gravity, hoth.radius)
-hoth_vol = planet_vol(hoth.radius)
+me.add('beef')
+me.add('chicken')
 
-print(hoth_mass, hoth_vol)
+me.print_bill(10, 10)
