41c0f72b175eb7b7a16d3ac07db4141c3a641861
zan <zan_zibert@hotmail.com>
2018-12-08 20:44:08 +0100

    Largest palindrome product

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def is_palindrome(number):
+    arr1 = list(str(number))
+    arr2 = list(reversed(arr1))
+    for i in range(len(arr1)):
+        if arr1[i] != arr2[i]:
+            return False
+    return True
 
-print(list(map(lambda n: n**2, nums)))
+biggest = 0
+
+for num1 in range(100, 1000):
+    for num2 in range(100, 1000):
+        result = num1 * num2
+        if is_palindrome(result):
+            if biggest < result:
+                biggest = result
+
+print(biggest)
