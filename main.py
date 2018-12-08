a27af5a7fa924ab4313074e1ace697206f647f88
zan <zan_zibert@hotmail.com>
2018-12-08 22:26:31 +0100

    Smallest multiple

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-counter = 3
+prime_dict = {
+    2: 0,
+    3: 0,
+    5: 0,
+    7: 0,
+    11: 0,
+    13: 0,
+    17: 0,
+    19: 0
+}
 
-primes = [2]
+primes = [2, 3, 5, 7, 11, 13, 17, 19]
 
-def is_prime(number, primes):
+for num in range(2, 21):
     for prime in primes:
-        if number % prime == 0:
-            return False
-    primes.append(number)
-    return True
+        counter = 0
+        while True:
+            if num % prime == 0:
+                counter += 1
+                num /= prime
+            else:
+                break
+        if counter > prime_dict[prime]:
+            prime_dict[prime] = counter
 
-counter = 1
-number = 3
+suma = 1
+for key, value in prime_dict.items():
+    if( value != 0):
+        suma *= (key ** value)
 
-while counter < 10001:
-    if is_prime(number, primes):
-        counter += 1
-    number += 1
-
-print(primes)
-
+print(suma)
     
