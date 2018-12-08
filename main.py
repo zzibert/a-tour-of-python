a8704cda3f95683bf5d87ff27434a1610f027966
zan <zan_zibert@hotmail.com>
2018-12-08 22:35:59 +0100

    Special Pythagorean triplet

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-prime_dict = {
-    2: 0,
-    3: 0,
-    5: 0,
-    7: 0,
-    11: 0,
-    13: 0,
-    17: 0,
-    19: 0
-}
-
-primes = [2, 3, 5, 7, 11, 13, 17, 19]
-
-for num in range(2, 21):
-    for prime in primes:
-        counter = 0
-        while True:
-            if num % prime == 0:
-                counter += 1
-                num /= prime
-            else:
-                break
-        if counter > prime_dict[prime]:
-            prime_dict[prime] = counter
-
-suma = 1
-for key, value in prime_dict.items():
-    if( value != 0):
-        suma *= (key ** value)
-
-print(suma)
-    
+for a in range(1, 1000):
+    for b in range(a + 1, 1000):
+        for c in range(b + 1, 1000):
+            if a + b + c != 1000:
+                continue
+            if a**2 + b**2 == c**2:
+                print(a*b*c)
