e703801f01b69a7971a1dd53cbed673fc3f91ea8
zan <zan_zibert@hotmail.com>
2018-12-08 17:12:30 +0100

    Python 3 Tutorial for Beginners #18 - Methods & Attributes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 class Planet:
+
+    shape = 'round'
+
     def __init__(self, name, radius, gravity, system):
         self.name = name
         self.radius = radius
@@ -8,5 +11,14 @@ class Planet:
     def orbit(self):
         return f'{self.name} is orbiting in the {self.system} system'
 
+    @classmethod
+    def commons(cls):
+        return f'All planets are {cls.shape}'
+
+    @staticmethod
+    def spin(speed = '2000 miles per hour'):
+        return f'The planet spins at {speed}'
+
+
 hoth = Planet("retard", 2000, 5.5, "yolo")
-print(hoth.orbit())
+print(hoth.spin())
