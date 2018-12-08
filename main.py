3092443bb52a07c4a6884d1be90d6a9e52949bd5
zan <zan_zibert@hotmail.com>
2018-12-08 13:35:49 +0100

    Python 3 Tutorial for Beginners #8 - If Statements

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-num1 = 3.12343545
-num2 = 10.234234234
+age = int(input('Enter your age: '))
 
-#previous
-#print('num 1 is', num1, '\nand num2 is', num2)
-
-#Format method
-#print('num 1 is {0:.3f} and num 2 is {1:.3f}'.format(num1, num2))
-
-#using f-strings
-print(f'num 1 is {num1} and num 2 is {num2}')
+if age < 10:
+    print("you are young")
+elif age < 40:
+    print("the fire in you is strong")
+else:
+    print("you are old")
