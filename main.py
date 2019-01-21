e1ff151b02df3fe30f81f0fa4bbe652fcc6a1c2a
zan <zan_zibert@hotmail.com>
2019-01-21 19:33:38 +0100

    Python 3 Tutorial for Beginners #4 - Strings

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-print(5 ** 5)
+greet = "hello"
 
-print(10 % 3)
+print(greet[-2])
 
-print(5 + 5 * 3)
+print(greet[0:3])
+
+print(greet[2:-1])
+
+greet = greet[0:3]
+
+print(greet)
+
+greet = "hello"
+
+str2 = "dudes"
+
+print(greet + " " + str2)
+
+print(greet * 3)
+
+print(greet.upper())
+
+cheese = "brie, chedder, stilton"
+
+print(cheese.split(', '))
+
+print(len(cheese))
