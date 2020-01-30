57ddbefc14f47cb44849ae22394c7d6e5d9a6c71
Žan Žiber <zan.zibert@ulu.io>
2020-01-30 15:45:28 +0100

    Multiples of 3 and 5

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-greet = "hello"
+counter = 1
+sum = 0
+while counter < 1000:
+    if counter % 3 == 0 or counter % 5 == 0:
+      sum += counter
+    counter += 1
 
-print(greet[-2])
-
-print(greet[0:3])
-
-print(greet[2:-1])
-
-greet = greet[0:3]
-
-print(greet)
-
-greet = "hello"
-
-str2 = "dudes"
-
-print(greet + " " + str2)
-
-print(greet * 3)
-
-print(greet.upper())
-
-cheese = "brie, chedder, stilton"
-
-print(cheese.split(', '))
-
-print(len(cheese))
+print(sum)
