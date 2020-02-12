96748b989d9c0ee4d722218e849a2cbc8495569f
Žan Žiber <zan.zibert@ulu.io>
2020-02-12 10:41:12 +0100

    88 merge sorted array

--- leetcode/88_merge_sorted_array/main.py
+++ leetcode/88_merge_sorted_array/main.py
@@ -1,14 +1,28 @@
-def merge(nums1, m, nums2, n):
-  index = 0
-  nums1 = nums1[0:(len(nums1) - n)]
-  for i in range(0, len(nums2)):
-    while index < len(nums1):
-      if nums2[i] <= nums1[index]:
-        nums1.insert(index, nums2[i])
-        break
-      else:
-        index += 1
-    return nums1
 
-print(merge([1,2,3,0,0,0], 3, [2,5,6], 3))
-    
+def merge(nums1, m, nums2, n):
+  
+  for i in range(n):
+    nums1.pop()
+  length = len(nums1)
+  i = 0
+  for number in nums2:
+    while True:
+      if length == 0:
+        nums1.append(number)
+        length += 1
+        break
+      if number <= nums1[i]:
+        nums1.insert(i, number)
+        length += 1
+        break
+      if i == (length-1):
+        nums1.append(number)
+        length += 1
+        break
+      i += 1
+  return nums1
+
+print(merge([1,2,3,0,0,0], 3, [2, 5, 6], 3))
+print(merge([0], 0, [1], 1))
+print(merge([4, 5, 6, 0, 0, 0], 3, [1, 2, 3], 3))
+print(merge([4,0,0,0,0,0], 1, [1,2,3,5,6], 5))
