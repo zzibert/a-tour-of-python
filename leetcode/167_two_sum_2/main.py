3c495104d46176ecce02bc859a71bb534af23fff
Žan Žiber <zan.zibert@ulu.io>
2020-02-12 09:39:01 +0100

    167 two sum 2

--- leetcode/167_two_sum_2/main.py
+++ leetcode/167_two_sum_2/main.py
@@ -2,11 +2,16 @@ def twoSum(numbers, target):
   values = {}
   indexes = {}
   for i in range(0, len(numbers)):
-    indexes[numbers[i]] = i
     desired = target - numbers[i]
-    values[numbers[i]] = desired
+    if desired == numbers[i]:
+      for j in range(i+1, len(numbers)):
+        if numbers[j] == desired:
+          return [i+1, j+1]
+    indexes[numbers[i]] = i
     if desired in values and values[desired] == numbers[i]:
       return [indexes[desired]+1, indexes[numbers[i]]+1]
+    values[numbers[i]] = desired
   
   
 print(twoSum([2,3,4], 6))
+print(twoSum([0,0, 3,4], 0))
